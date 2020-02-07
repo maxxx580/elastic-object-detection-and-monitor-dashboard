82d82659333ff80bb66c3b4b2f914d17894c4002
Zixiang Ma <ericma0824@gmail.com>
2020-02-06 23:37:19 -0500

    setup.py

--- app/db.py
+++ app/db.py
@@ -4,12 +4,13 @@ from flask import current_app, g
 from flask.cli import with_appcontext
 
 config = {
-        'username':'root',
-        'password':'MYD0622',
-        'host':"127.0.0.1",
-        'port':3306,
-        'db':'ece1779'
+    'username': 'root',
+    'password': 'password',
+    'host': "127.0.0.1",
+    'port': 3306,
+    'db': 'ece1779'
 }
+
 
 def get_db():
     """Connect to the application's configured database. The connection
@@ -18,12 +19,13 @@ def get_db():
     """
     if "db" not in g:
         g.db = mysql.connector.connect(
-                user=config['username'],
-                password=config['password'],
-                host=config['host'],
-                database=config['db'],
-                port=config['port'])
+            user=config['username'],
+            password=config['password'],
+            host=config['host'],
+            database=config['db'],
+            port=config['port'])
     return g.db
+
 
 def close_db(e=None):
     """If this request connected to the database, close the
@@ -34,12 +36,14 @@ def close_db(e=None):
     if db is not None:
         db.close()
 
+
 def init_db():
     """Clear existing data and create new tables."""
     db = get_db()
     cur = db.cursor()
     with current_app.open_resource("setup.sql") as f:
         cur.execute(f.read().decode("utf8"), multi=True)
+
 
 @click.command("init-db")
 @with_appcontext
