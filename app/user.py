e3230ec0129f7b4708081904a83f27db6e950a6b
Hongyu Liu <hongyu.liu@mail.utoronto.ca>
2020-02-06 22:00:03 -0500

    add some error handler

--- app/user.py
+++ app/user.py
@@ -1,20 +1,13 @@
 import functools
+import logging
 
-from flask import Blueprint
-from flask import flash
-from flask import g
-from flask import jsonify
-from flask import redirect
-from flask import render_template
-from flask import request
-from flask import session
-from flask import url_for
+import bcrypt
+from flask import (Blueprint, flash, g, jsonify, redirect, render_template,
+                   request, session, url_for,abort)
+
 from werkzeug.exceptions import abort
-from werkzeug.security import generate_password_hash
 
-from werkzeug.security import check_password_hash
-from app.db import get_db, close_db
-
+from app.db import close_db, get_db
 
 bp = Blueprint("user", __name__, url_prefix='/user')
 
@@ -24,10 +17,11 @@ def login_required(view):
 
     @functools.wraps(view)
     def wrapped_view(**kwargs):
+        logger = logging.getLogger()
         if g.user is None:
+            logger.info("user yet logged in, redirecting log-in page")
             return redirect(url_for("user.login"))
-
-        print("user already logged in")
+        logger.info('user already logged in')
         return view(**kwargs)
 
     return wrapped_view
@@ -47,7 +41,6 @@ def load_logged_in_user():
 @bp.route("/register", methods=("GET", "POST"))
 def register():
     """Register a new user.
-
     Validates that the username is not already taken. Hashes the
     password for security.
     """
@@ -56,40 +49,27 @@ def register():
         return render_template('user/register.html')
 
     username = request.form['username']
-    password = request.form['password']
+    password = request.form['password'].encode('utf-8')
 
     try:
 
-        assert(username is not None)
-        assert(password is not None)
+        assert username is not None, "Please enter username"
+        assert password is not None, "Please enter password"
 
-<<<<<<< Updated upstream
-        cnx=get_db()
-        db_cursor = cnx.cursor()
-        db_cursor.execute('SELECT * FROM user WHERE username="%s"' % (username))
-        user = db_cursor.fetchone()
+        assert len(username) < 100, "Username should be less than 100 characters"
 
-        assert(user is not None,"user exists")
-        salt = generate_password_hash(password,method='pbkdf2:sha256:20')
-        query = 'INSERT INTO user (username, password,salt) VALUES (%s,%s,%s)'
-        db_cursor.execute(query,(username,password,salt))
-=======
-        assert len(username) <=100, "Username should be less than 100 characters"
         cnx = get_db()
         db_cursor = cnx.cursor()
-        salt = bcrypt.gensalt()
-        pw_hashed = bcrypt.hashpw(password, salt)
 
-        db_cursor = get_db().cursor()
         db_cursor.execute(
             'select * from user where username="%s"' % (username))
         user = db_cursor.fetchone()
-
         assert user is None, "Username exists"
 
+        salt = bcrypt.gensalt()
+        pw_hashed = bcrypt.hashpw(password, salt)
         query = 'INSERT INTO user (username, password) VALUES (%s,%s)'
         db_cursor.execute(query, (username, pw_hashed))
->>>>>>> Stashed changes
 
         cnx.commit()
 
@@ -106,7 +86,7 @@ def register():
 
 @bp.route('login', methods=['GET', 'POST'])
 def login():
-
+    logger = logging.getLogger()
     if request.method == 'GET':
         return render_template('user/login.html')
 
@@ -115,46 +95,38 @@ def login():
 
     try:
 
-        assert(username is not None)
-        assert(password is not None)
+        assert username is not None, "invalid user name"
+        assert password is not None, "invalid password"
 
         db_cursor = get_db().cursor()
         db_cursor.execute(
             'select * from user where username="%s"' % (username))
         user = db_cursor.fetchone()
 
-        assert(user is not None)
-
-        # uncommet after sign in endpoint compelted
-        # assert(check_password_hash(user[1], password))
-        assert(password == 'password')
+        assert user is not None, "invalid credential"
+        assert bcrypt.checkpw(password.encode('utf-8'),
+                              user[1].encode('utf-8')), "invalid credential"
 
         session.clear()
         session.permanent = True
+
         session['username'] = username
 
         return redirect(url_for('index'))
 
+    except AssertionError as e:
+        flash(e)
+        logger.error(e.args)
+        return render_template('user/login.html', e=e.args)
+
     except Exception as e:
-        print(e)
+        flash(e)
+        logger.error(e)
         return render_template('user/login.html')
 
 
-@bp.route("/")
-def get_users():
-    return render_template('user/index.html')
-
-
-# db connection test only
-@bp.route('/all')
-def get_all_users():
-    db_connection = get_db()
-    cursor = db_connection.cursor()
-    cursor.execute('select * from User;')
-    rows = cursor.fetchall()
-    return jsonify(rows)
-
 @bp.route("/logout")
+@login_required
 def logout():
     """Clear the current session, including the stored user id."""
     session.clear()
--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-mysql -u root "-pMYD0622" ece1779 < app/setup.sql
+mysql -u root "-ppassword" ece1779 < app/setup.sql
 . venv/bin/activate
 pip install -r requirements.txt
 gunicorn -w 4 -b 127.0.0.1:4000 wsgi:webapp
