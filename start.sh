1f51bb10ec7b310e5d020db6021cd2fd9bd0765c
Zixiang Ma <ericma0824@gmail.com>
2020-02-09 12:03:10 -0500

    fix modudle not found

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-mysql -u root "-pece1779pass" ece1779 < app/setup.sql
+mysql -u root "-ppassword" ece1779 < app/setup.sql
 . venv/bin/activate
-pip install .
+pip install -r requirements.txt
 gunicorn -w 4 -b 127.0.0.1:8000 wsgi:webapp
