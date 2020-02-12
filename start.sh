c7bf31a849c39814325eed4e7b2bcc74b1ec21c2
Zixiang Ma <ericma0824@gmail.com>
2020-02-12 10:38:26 -0500

    updated number of worker

--- start.sh
+++ start.sh
@@ -2,4 +2,4 @@
 mysql -u root "-ppassword" < app/setup.sql
 . venv/bin/activate
 pip3 install -r requirements.txt
-gunicorn -w 4 -b 127.0.0.1:8000 wsgi:webapp
+gunicorn -w 12 -b 127.0.0.1:8000 wsgi:webapp
