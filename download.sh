287a08068ad2d9012d06b7a478ba3cb4a57ca36d
Ubuntu <ubuntu@ip-172-31-47-219.ec2.internal>
2020-02-12 03:40:03 +0000

    download script

---
+++ download.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+fileid="1C6SoDNTcWRSCPDD1zbrECIbRtqXMUSD_"
+filename="yolov3.weights"
+curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=${fileid}" > /dev/null
+curl -Lb ./cookie "https://drive.google.com/uc?export=download&confirm=`awk '/download/ {print $NF}' ./cookie`&id=${fileid}" -o ${filename}
