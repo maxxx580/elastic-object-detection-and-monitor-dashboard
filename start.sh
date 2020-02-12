1a8547d25c09f284c31cb967d097c832c030a99c
Ubuntu <ubuntu@ip-172-31-47-219.ec2.internal>
2020-02-12 19:24:00 +0000

    reduce thread

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-mysql -u root "-ppassword" < app/setup.sql
+mysql -u root "-pece1779pass" < app/setup.sql
 mkdir app/static
 mkdir app/static/uploaded_images
 . venv/bin/activate
 pip3 install -r requirements.txt
-gunicorn -w 12 -b 127.0.0.1:8000 wsgi:webapp
+gunicorn -w 3 -b 127.0.0.1:8000 wsgi:webapp --timeout 90 --graceful-timeout 60
