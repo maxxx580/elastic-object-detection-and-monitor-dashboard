1382656b3265199c6a34120c16e18cd93a60bcd9
Zixiang Ma <ericma0824@gmail.com>
2020-02-12 15:41:31 -0500

    update readme

--- start.sh
+++ start.sh
@@ -4,4 +4,4 @@ mkdir app/static
 mkdir app/static/uploaded_images
 . venv/bin/activate
 pip3 install -r requirements.txt
-gunicorn -w 12 -b 127.0.0.1:8000 wsgi:webapp
+gunicorn -w 3 -b 127.0.0.1:5000 wsgi:webapp
