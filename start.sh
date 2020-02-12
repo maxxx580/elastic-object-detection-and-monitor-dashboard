7a0395cfa1190808c407f46600433f8d51891d4a
Ubuntu <ubuntu@ip-172-31-47-219.ec2.internal>
2020-02-12 03:49:44 +0000

    deploy fixup

--- start.sh
+++ start.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 mysql -u root "-pece1779pass" < app/setup.sql
+mkdir app/static
+mkdir app/static/uploaded_images
 . venv/bin/activate
 pip3 install -r requirements.txt
 gunicorn -w 4 -b 127.0.0.1:8000 wsgi:webapp
