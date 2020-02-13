f0f82d89ec5a89a833616823d9d9cfd94569ccfa
Zixiang Ma <ericma0824@gmail.com>
2020-02-13 13:22:03 -0500

    project clean up

--- setup.py
+++ setup.py
@@ -1,28 +1,22 @@
 from setuptools import find_packages, setup
+try:
+    # for pip >= 10
+    from pip._internal.req import parse_requirements
+except ImportError:
+    # for pip <= 9.0.3
+    from pip.req import parse_requirements
+
+
+def load_requirements(fname):
+    reqs = parse_requirements(fname, session='installation')
+    return [str(ir.req) for ir in reqs]
+
 
 setup(
-    name='app',
+    name='ece1779-a1',
     version='0.9.9',
     packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
-    install_requires=[
-        'Flask',
-        'cffi',
-        'Click',
-        'dnspython',
-        'Flask-Bcrypt',
-        'gunicorn',
-        'itsdangerous',
-        'Jinja2',
-        'MarkupSafe',
-        'mysql-connector-python',
-        'numpy',
-        'opencv-contrib-python',
-        'protobuf',
-        'pycparser',
-        'pylint',
-        'six',
-        'Werkzeug'
-    ]
+    install_requires=load_requirements('setup/requirements.txt')
 )
--- start.sh
+++ start.sh
@@ -1,7 +1,14 @@
 #!/bin/sh
-mysql -u root "-ppassword" < setup.sql
+mysql -u root "-ppassword" < setup/setup.sql
 mkdir -p app/static
 mkdir -p app/static/uploaded_images
+{
+    if [ ! -f /app/yolo-coco/yolov3.weights ]; then
+    echo "yolov3.weights exits"
+    else
+        bash setup/download.sh
+    fi
+}
 . venv/bin/activate
-pip3 install -r requirements.txt
+pip3 install .
 gunicorn -w 3 -b 127.0.0.1:5000 wsgi:webapp
