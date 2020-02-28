7454a554fec008a610ddc0e25d0e71b3faf25a19
Zixiang Ma <ericma0824@gmail.com>
2020-02-28 13:41:44 -0500

    completed chart number of workers in the past 30 min

--- manager/__init__.py
+++ manager/__init__.py
@@ -7,6 +7,7 @@ from flask import Flask, url_for, redirect, render_template
 from flask_caching import Cache
 from manager import workers
 from manager.aws import autoscale
+from datetime import datetime
 
 from apscheduler.schedulers.background import BackgroundScheduler
 
@@ -27,23 +28,36 @@ def create_app():
     def dashboard():
         return render_template('dashboard.html')
 
-    @app.route('/workers_tracking')
-    def workers_tracking():
-        return render_template('workers_tracking.html')
+    @app.route('/workers_dashboard')
+    def workers_dashboard():
+        return render_template('workers_dashboard.html')
+
+    @app.route('/logint', methods=['POST'])
+    def login():
+        pass
+
+    @app.route('/logout', methods=['POST'])
+    def logout():
+        pass
+
+    @app.route('/terminate', methods=['POST'])
+    def terminate():
+        pass
 
     def update_worker_pool_size():
         if len(worker_pool_size) > 30:
             worker_pool_size.pop(0)
-        worker_pool_size.append(len(auto_scaler.worker_pool))
+        worker_pool_size.append(
+            (len(auto_scaler.worker_pool), datetime.utcnow()))
         print("##############Updating worker pool size: %d ############" %
-              worker_pool_size[-1])
+              worker_pool_size[-1][0])
 
+    update_worker_pool_size()
     scheduler = BackgroundScheduler()
     scheduler.add_job(func=update_worker_pool_size,
                       trigger="interval", seconds=60)
     scheduler.start()
 
-    # Shut down the scheduler when exiting the app
     atexit.register(lambda: scheduler.shutdown())
 
     return app
