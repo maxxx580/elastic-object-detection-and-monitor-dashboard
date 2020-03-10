524b17cc2ad34e4333c9640293fe8e1323caf194
Zixiang Ma <ericma0824@gmail.com>
2020-03-10 18:06:43 -0400

    fix manager port number

--- start_manager.sh
+++ start_manager.sh
@@ -7,4 +7,4 @@
 
 source venv/bin/activate
 pip3 install -r setup/requirements.txt
-gunicorn -w 1 -b 0.0.0.0:5001 run_manager:manager_app --timeout 120
+gunicorn -w 1 -b 0.0.0.0:5000 run_manager:manager_app --timeout 120
