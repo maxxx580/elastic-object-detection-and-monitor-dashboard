1ba15b464479acc93fd25a74c9fa193bb340e7e6
Zixiang Ma <ericma0824@gmail.com>
2020-03-13 22:33:15 -0400

    new ami id

--- manager/aws/instance_manager.py
+++ manager/aws/instance_manager.py
@@ -18,7 +18,7 @@ class InstanceManager:
 
         self.user_app_tag = 'ece1779-a2-worker'
         self.manager_app_tag = 'ece1779-a2-manager'
-        self.image_id = 'ami-0dcc3df7ad2533c0f'
+        self.image_id = 'ami-0016a2318cd331666'
         self.instance_type = 't2.small'
         self.key_pair = 'keypair'
         # rachel's elb
